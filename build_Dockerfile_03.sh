001b91fc81231f5a84d18f9f145bb57cedc70722
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-10-27 21:20:57 +0200

    Image 3 does not need to be squashed

--- build_Dockerfile_03.sh
+++ build_Dockerfile_03.sh
@@ -2,6 +2,4 @@
 
 # Build user setup
 docker build -t tlinnet/relax:03 -f Dockerfile_03_user_setup .
-ID=$(docker run -d tlinnet/relax:03 /bin/bash)
-docker export $ID | docker import - tlinnet/relax:03
 alias dr3='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/home/developer/work --name relax03 tlinnet/relax:03'
