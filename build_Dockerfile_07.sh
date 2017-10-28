9c00b581a61d615b3dd1a3317425c8a2c5f81ad0
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-10-28 11:23:11 +0200

    Moved bruker compassXport to own Docker file. This needs some work with wine

---
+++ build_Dockerfile_07.sh
@@ -0,0 +1,5 @@
+# To build this image, source the file
+
+# Build with bruker CompassXport
+docker build -t tlinnet/relax:07 -f Dockerfile_07_bruker_CompassXport .
+alias dr7='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/home/developer/work --name relax07 tlinnet/relax:07'
