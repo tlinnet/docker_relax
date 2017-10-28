dff60de3db3a7526cae2b1e285af29fc3ae3439f
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-10-28 10:29:28 +0200

    Make alias jupyter

--- build_Dockerfile_02.sh
+++ build_Dockerfile_02.sh
@@ -4,4 +4,4 @@
 docker build -t tlinnet/relax:02 -f Dockerfile_02_python .
 alias dr2='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/home/developer/work --name relax02 tlinnet/relax:02'
 # Docker relax Jupyter notebook
-alias drn='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/home/developer/work -p 8888:8888 --name relax tlinnet/relax jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0'
+alias dr2n='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/home/developer/work -p 8888:8888 --name relax02 tlinnet/relax02 jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0'
