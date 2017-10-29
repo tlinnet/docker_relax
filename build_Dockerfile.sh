edad102e90937fd6f436bd8ed3be6f83cc96378a
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-10-29 12:00:00 +0100

    Not building the CompassXport

--- build_Dockerfile.sh
+++ build_Dockerfile.sh
@@ -28,11 +28,6 @@ alias dr5='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X
 docker build -t tlinnet/relax:06 -f Dockerfile_06_mmass .
 alias dr6='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD":/home/developer/work --name relax06 tlinnet/relax:06'
 
-# Build with bruker CompassXport
-docker build -t tlinnet/relax:07 -f Dockerfile_07_bruker_CompassXport .
-alias dr7='docker run -ti --rm -e DISPLAY=$(ipconfig getifaddr en1):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD":/home/developer/work --name relax07 tlinnet/relax:07'
-
-
 
 # Build relax
 docker build -t tlinnet/relax:10 -f Dockerfile_10_relax .
