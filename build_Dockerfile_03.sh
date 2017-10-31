761b2135990b3b43b6e64a251c1b44399bd619bf
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-10-31 23:28:28 +0100

    Trying to add widgets for JupyterLab

--- build_Dockerfile_03.sh
+++ build_Dockerfile_03.sh
@@ -6,3 +6,4 @@ alias dr3='docker run -ti --rm -e DISPLAY=$(ifconfig|grep "inet "|grep -v 127.0.
 # Docker relax Jupyter notebook
 alias dr3j='docker run -ti --rm -e DISPLAY=$(ifconfig|grep "inet "|grep -v 127.0.0.1|cut -d" " -f2):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD":/home/developer/work -p 8888:8888 --name relax03 $USER/relax:03_user_setup'
 alias dr3n='docker run -ti --rm -e DISPLAY=$(ifconfig|grep "inet "|grep -v 127.0.0.1|cut -d" " -f2):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD":/home/developer/work -p 8888:8888 --name relax03 $USER/relax:03_user_setup jupyter-notebook --debug --no-browser --port 8888 --ip=0.0.0.0'
+alias dr3l='docker run -ti --rm -e DISPLAY=$(ifconfig|grep "inet "|grep -v 127.0.0.1|cut -d" " -f2):0 -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD":/home/developer/work -p 8888:8888 --name relax03 $USER/relax:03_user_setup jupyter-lab --debug --no-browser --port 8888 --ip=0.0.0.0'
